e94de01304272f2cef0fcd9c95391fb1c4ba02b1
mikeZ98 <145975972+mikeZ98@users.noreply.github.com>
2025-09-17 10:25:19 +0200

    Delete quickstart.sh

--- quickstart.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -m venv .venv
-source .venv/bin/activate || source .venv/Scripts/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-echo 'Environment ready. Launching Jupyter Lab...'
-jupyter lab
